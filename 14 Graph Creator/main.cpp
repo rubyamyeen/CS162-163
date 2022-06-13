a8f159c914f7951d53becd56a4e0f3c54b770333
Ruby Amyeen <rsamyeen@gmail.com>
2022-06-12 19:46:04 -0700

    remove functions

--- 14 Graph Creator/main.cpp
+++ 14 Graph Creator/main.cpp
@@ -26,56 +26,126 @@ int main() {
       graph[i][j] = 0;
     }
   }
-
+  cout << "For this program the commands are:\n"
+       << "AV to add a vertex\n"
+       << "AE to add a edge\n"
+       << "RV to remove a vertex\n"
+       << "RE to remove a edge\n"
+       << "SP to find shortest path\n"
+       << "PRINT to print\n"
+       << "QUIT to exit the program"
+       << endl;
   while (stillRunning == true) {
-    cout << "Please enter: 'ADDVERTEX', 'ADDEDGE', "
-	 << "'REMOVEVERTEX', 'REMOVEEDGE', 'PRINT', or 'QUIT'" << endl;
+    cout << "Please enter a command:" << endl;
     cin >> input;
 
     //add vertex
-    if (strcmp(input, "ADDVERTEX") == 0) {
+    if (strcmp(input, "AV") == 0) {
       cout << "Enter a label for a vertex (ex: A):"  << endl;
       cin >> vertices[vCount];
       vCount++;
-      for (int i = 0; i < vCount; i++) {
-	cout << vertices[i] << " ";
-      }
-      cout << endl;
       
     // add edge
-    } else if (strcmp(input, "ADDEDGE") == 0) {
+    } else if (strcmp(input, "AE") == 0) {
+      //vars
       char v1;
       char v2;
+      bool used = false;
+      bool used2 = false;
+      int vIndex1 = 0;
+      int vIndex2 = 0;
+      int weight = 0;
+      
       cout << "Enter the two vertices you want to connect (ex: A B):" << endl;
       cin >> v1;
       cin >> v2;
-      bool used = false;
-      bool used2 = false;
+
 
       //check if vertices are in the matrix
       if (v1 != v2) {
 	for (int i = 0; i < vCount; i++) {
 	  if (vertices[i] == v1) {
 	    used = true;
+	    vIndex1 = i;
 	  } else if (vertices[i] == v2) {
 	    used2 = true;
+	    vIndex2 = i;
 	  }
 	}
       }
 
       if (used == true && used2 == true) {
-	cout << "valid input" << endl;
+	cout << "Enter a weight value:" << endl;
+	cin >> weight;
+	graph[vIndex1][vIndex2] = weight;
+	
+      } else {
+	cout << "Invalid input" << endl;
       }
 
       
     //remove vertex
-    } else if (strcmp(input, "REMOVEVERTEX") == 0) {
-
+    //https://stackoverflow.com/questions/879603/remove-an-array-element-and-shift-the-remaining-ones
+    } else if (strcmp(input, "RV") == 0) {
+      bool used = false;
+      char v;
+      cout << "Enter a vertex to remove" << endl;
+      cin >> v;
+      //check if inside the matrix
+      for (int i = 0; i < 20; i++) {
+	if (vertices[i] == v) {
+	  used = true;
+	  //shift next element over with vertices
+	  for (int j = i; j < vCount; j++) {
+	    vertices[j] = vertices[j+1];
+	  }
+	  //shift for graph
+	  for (int k = i; k < vCount; k++) {
+	    for (int l = i; l < vCount; l++) {
+	      graph[l][k] = graph[l][k + 1];
+	    }
+	    for (int m = i; m < vCount; m++) {
+	      graph[k][m] = graph[k + 1][m];
+	    }
+	  }
+	  vCount--;
+	}
+      }
       
     //remove edge 
-    } else if (strcmp(input, "REMOVEEDGE") == 0) {
+    } else if (strcmp(input, "RE") == 0) {
+      char v1;
+      char v2;
+      bool used = false;
+      bool used2 = false;
+      int vIndex1 = 0;
+      int vIndex2 = 0;
+      
+      cout << "Enter two vertices to remove the edge between them (ex: A B):" << endl;
+      cin >> v1;
+      cin >> v2;
 
 
+      //check if vertices are in the matrix
+      if (v1 != v2) {
+	for (int i = 0; i < vCount; i++) {
+	  if (vertices[i] == v1) {
+	    used = true;
+	    vIndex1 = i;
+	  } else if (vertices[i] == v2) {
+	    used2 = true;
+	    vIndex2 = i;
+	  }
+	}
+      }
+
+      if (used == true && used2 == true) {
+	//remove edge
+	graph[vIndex1][vIndex2] = 0;
+	
+      } else {
+	cout << "Invalid input" << endl;
+      }
       
     //print (Referenced Fiona Wang's code)
     } else if (strcmp(input, "PRINT") == 0) {
@@ -94,19 +164,18 @@ int main() {
 	}
 	cout << endl;
 	//prints out the label and its corresponding edges
-	for (int i = 0; i < 20; i++) {
-	  if (vertices[i] != '\0') {
-	    cout << vertices[i] << "\t";
-	    for (int j = 0; j < 20; j++){
-	      if (j < vCount) {
-		cout << graph[j][i] << "\t";
-	      }
-	    }
-	    cout << endl;
+	for (int i = 0; i < vCount; i++) {
+	  cout << vertices[i] << "\t";
+	  for (int j = 0; j < vCount; j++){
+	    cout << graph[j][i] << "\t";
 	  }
+	  cout << endl;
 	}
       }
  
+    //shortest path
+    } else if (strcmp(input, "SP") == 0) {
+
     //quit
     } else if (strcmp(input, "QUIT") == 0) {
       stillRunning = false;
